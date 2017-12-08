4b997902b831cb4b873afe0f3aafebd2a4fa5f4e
Musa Tokgöz <tokgozmusa@gmail.com>
2017-12-09 00:10:39 +0300

    Fix indentation

--- Example6.java
+++ Example6.java
@@ -3,14 +3,11 @@
 
 public class Example6 {
     public static void main(String[] args) {
-        for(int i = 1;i<10;i++){
+        for(int i = 1;i<=10;i++){
             System.out.println("-------------------");
-            for (int j = 1;j<10;j++){
+            for (int j = 1;j<=10;j++){
                 System.out.println(i+" X "+j+" = "+(i*j));
-        }
-         
-            
+            }
         }
     }
-    
 }
